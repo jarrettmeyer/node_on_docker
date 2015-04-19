0de311388c5406cc2530cddada54ad5b5a1eb5f1
Jarrett Meyer <jarrettmeyer@gmail.com>
2015-04-19 11:57:40 -0400

    Fixes start_app script.

--- start_app.sh
+++ start_app.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-vagrant ssh /home/vagrant/node_on_docker/couchdb/build.sh
-vagrant ssh /home/vagrant/node_on_docker/couchdb/run.sh
-vagrant ssh /home/vagrant/node_on_docker/node_api/build.sh
+vagrant ssh -c /home/vagrant/docker_demo/couchdb/build.sh
+vagrant ssh -c /home/vagrant/docker_demo/couchdb/run.sh
+vagrant ssh -c /home/vagrant/docker_demo/node_api/build.sh
+vagrant ssh -c /home/vagrant/docker_demo/node_api/run.sh
+vagrant ssh -c /home/vagrant/docker_demo/client_app/build.sh
+vagrant ssh -c /home/vagrant/docker_demo/client_app/run.sh
